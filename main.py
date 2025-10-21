c460f8f8adcf095bd26a5e9018e193803b669451
Štofanová <stofanovamichaela@gmail.com>
2025-10-21 12:51:15 +0200

    Zmena cisel 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from tvary import obdlznik
 import tvary.kruh
-print(f"Obvod obdĺžnika o veľkosti 10 x 15 je {obdlznik.obvod(10, 15)}")
-print(f"Obvod kruhu o veľkosti 10 je {tvary.kruh.obvod(10)}")
+print(f"Obvod obdĺžnika o veľkosti 10 x 40 je {obdlznik.obvod(10, 40)}")
+print(f"Obvod kruhu o veľkosti 20 je {tvary.kruh.obvod(20)}")
